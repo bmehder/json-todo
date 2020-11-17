d8ca08ffb8990af54f1c928aaf15236de9620341
Brad Mehder <bmehder@gmail.com>
2020-11-16 20:31:35 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Users from "./Users.svelte";
+</script>
+
+<style>
+  h1 {
+    margin: 10px 0;
+  }
+</style>
+
+<h1>Reactive ToDo List via JSON Object</h1>
+<Users />
---
+++ src/Users.svelte
@@ -0,0 +1,50 @@
+<script>
+  import User from "./User.svelte";
+  import { users } from "./data-users.js";
+
+  const addUser = () => {
+    const id = Math.floor(Math.random() * 1000);
+    let name = "";
+    $users["ID" + id] = { id, name };
+  };
+</script>
+
+<style>
+  #users {
+    display: grid;
+    grid-gap: 10px;
+    margin: 10px 0;
+    padding: 1em 2em;
+    background: white;
+    border-radius: 10px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.24);
+  }
+  button {
+    height: 45px;
+    margin-bottom: 1em;
+  }
+  aside {
+    background: white;
+    padding: 1em 2em;
+    border-radius: 10px;
+  }
+  h3 {
+    text-align: center;
+    margin-bottom: 1em;
+  }
+</style>
+
+<div id="users">
+  <h3>Form (Frontend)</h3>
+  {#each Object.keys($users) as key}
+    <User {key} bind:user={$users[key]} />
+  {/each}
+
+  <button on:click={addUser}>Add User</button>
+
+</div>
+
+<aside>
+  <h3>DataStore (backend)</h3>
+  <pre>{JSON.stringify($users, null, 2)}</pre>
+</aside>
